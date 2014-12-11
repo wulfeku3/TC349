c34fa672357d2efa7f31ab525250e6ac4bf46335
Erika Wulfekuhler <wulfeku3@msu.edu>
2014-12-10 23:14:30 -0500

    final project

---
+++ final/js/final.js
@@ -0,0 +1,92 @@
+function onPressImage1()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo1.jpg";
+	
+	document.getElementById("description").innerHTML = "The Shoemaker in 'The Shoemaker and the Elves'"
+}
+
+function onPressImage2()
+{
+	// finds the large image element
+	var imageElement = document.getElementById("bigPicture");
+	
+	//sets its source to the new image
+	imageElement.src = "images/photo2.jpg";
+	
+	document.getElementById("description").innerHTML = "The Artful Dodger and Oliver in 'Oliver Twist'"
+}
+
+function onPressImage3()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo3.jpg";
+	
+	document.getElementById("description").innerHTML = "Cast of 'A Walk in the Woods'"
+}
+
+function onPressImage4()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo4.jpg";
+	
+	document.getElementById("description").innerHTML = "Cast of 'The Emperor's Nightingale'"
+}
+
+
+function onPressImage5()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo5.jpg";
+	
+	document.getElementById("description").innerHTML = "Mole, Rat, Badger, and Toad in 'Mr. Toad's Mad Adventures'"
+}
+
+function onPressImage6()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo6.jpg";
+	
+	document.getElementById("description").innerHTML = "Closet Monsters and the Dream King in 'Monster in the Closet'"
+}
+
+function onPressImage7()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo7.jpg";
+	
+	document.getElementById("description").innerHTML = "Murray the Monster of 'Monster in the Closet'"
+}
+
+function onPressImage8()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo8.jpg";
+	
+	document.getElementById("description").innerHTML = "Orphans behind the scenes of 'Oliver Twist'"
+}
+
+function onPressImage9()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo9.jpg";
+	
+	document.getElementById("description").innerHTML = "Habibi Dancers in 'Aladdin'"
+}
+
+function onPressImage10()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "images/photo10.jpg";
+	
+	document.getElementById("description").innerHTML = "Charlie and Leroy in 'The Best Christmas Pagaent Ever'"
+}
